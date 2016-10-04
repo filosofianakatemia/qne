79fa4a1964f096fd7cb8dd87fde43b288d87ae37
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2016-10-04 11:02:25 +0300

    Upgraded Angular 2, added placeholder for simple guard.

---
+++ respondent/src/app/app.guard.ts
@@ -0,0 +1,64 @@
+import 'rxjs/add/operator/take';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/let';
+import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Router, CanActivate, ActivatedRouteSnapshot } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+
+import { QuestionnaireService } from './questionnaire/questionnaire.service';
+
+/**
+ * Guards are hooks into the route resolution process, providing an opportunity
+ * to inform the router's navigation process whether the route should continue
+ * to activate this route. Guards must return an observable of true or false.
+ */
+/*@Injectable()
+export class QuestionnaireExistsGuard implements CanActivate {
+  constructor(
+    private store: Store<fromRoot.State>,
+    private questionnaireService: QuestionnaireService,
+    private router: Router
+  ) { }
+
+  /**
+   * This method loads a book with the given ID from the API and caches
+   * it in the store, returning `true` or `false` if it was found.
+   */
+  /*
+  getQuestionnaireFromAPI(id: questionnaireId): Observable<boolean> {
+    return this.googleBooks.retrieveBook(id)
+      .map(bookEntity => new book.LoadAction(bookEntity))
+      .do(action => this.store.dispatch(action))
+      .map(book => !!book)
+      .catch(() => {
+        this.router.navigate(['/404']);
+        return of(false);
+      });
+  }
+*/
+  /**
+   * This is the actual method the router will call when our guard is run.
+   *
+   * Our guard waits for the collection to load, then it checks if we need
+   * to request a book from the API or if we already have it in our cache.
+   * If it finds it in the cache or in the API, it returns an Observable
+   * of `true` and the route is rendered successfully.
+   *
+   * If it was unable to find it in our cache or in the API, this guard
+   * will return an Observable of `false`, causing the router to move
+   * on to the next candidate route. In this case, it will move on
+   * to the 404 page.
+   */
+/*
+  canActivate(route: ActivatedRouteSnapshot): Observable<boolean> {
+    return this.waitForCollectionToLoad()
+      .switchMap(() => this.hasBook(route.params['id']));
+  }
+}
+*/
