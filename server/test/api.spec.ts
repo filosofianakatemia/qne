11d89eadd89148e9d30db690bc6a1a06154fbacb
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2016-11-09 16:28:07 +0200

    Added missing files.

---
+++ server/test/api.spec.ts
@@ -0,0 +1,30 @@
+import * as chakram from "chakram";
+const expect = chakram.expect;
+
+describe("qne server API", () => {
+  let pollingInterval;
+  before(function (done) {
+    // Set one minute as timeout for tests
+    this.timeout(60000);
+    pollingInterval = setInterval(function(){
+      chakram.get("http://localhost:3000/v1").then(function(response){
+        if (!response.error){
+          clearInterval(pollingInterval);
+          done();
+        }
+      });
+    }, 500);
+  });
+
+  it("should return valid questions from GET qne-api/v1/questions/test-questionnaire", function () {
+    const questionnairesPromise = chakram.get("http://localhost:3000/qne-api/v1/questions/test-questionnaire");
+
+    // TODO: Use something like this when JSON is returned from the server
+    // return expect(questionnaires).to.have.json('with[0].content', questionnaires);
+
+    return questionnairesPromise.then(function(response){
+      expect(JSON.stringify(response.body)).to.equal(
+        '{"uuid":"12345-1231233121","title":"Test questionnaire","submit":"123214234","path":"test-questionnaire"}');
+    });
+  });
+});
---
+++ server/test/typings.d.ts
@@ -0,0 +1,8 @@
+// typings.d.ts
+
+// There are no typings yet for Chakram, waiting for https://github.com/dareid/chakram/issues/33
+
+declare module 'chakram' {
+  let x: any;
+  export = x;
+}
