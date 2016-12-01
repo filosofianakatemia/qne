9427f6a1d08e79905913dc77ff5e5412482fada6
Aleksanteri Rytkonen <alrytkonen@gmail.com>
2016-12-01 15:46:21 +0200

    modified progressbar to correctly show current position

--- respondent/src/app/shared/progressbar.component.ts
+++ respondent/src/app/shared/progressbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+import {Component, Input, Output, EventEmitter, OnInit, OnChanges, SimpleChanges} from '@angular/core';
 import { UIGroup } from '../action/ui-group.model';
 import { QuestionGroup } from '../group/group.model';
 import { AnswerValue } from '../action/answer-value.model';
@@ -9,15 +9,15 @@ import { AnswerValue } from '../action/answer-value.model';
         <div class="row">
         <div class="large-8 columns">
         <div class="progress"  role="progressbar" tabindex="0" aria-valuenow="20" aria-valuemin="0" aria-valuetext="25 percent" aria-valuemax="100">
-          <span class="progress-meter">
-            <p class="progress-meter-text">{{answeredQuestions.length}}/{{amountOfQuestions.length}}</p>
+          <span class="progress-meter"[style.width]="progBarLength">
+            <p class="progress-meter-text">{{answeredQuestions.length}}/{{elems.length}}</p>
           </span>
         </div>
         </div>
         <div class="large-4 columns">
         <div class="expanded button-group">
-            <button class="button" [disabled]="hidePrevButton" (click)="navigate.emit(-1)" (click)="questionNumberMinusOne()">Previous</button>
-            <button class="button" [disabled]="hideNextButton" (click)="navigate.emit(1)" (click)="questionNumberPlusOne()">Next</button>
+            <button class="button" [disabled]="hidePrevButton" (click)="navigate.emit(-1)">Previous</button>
+            <button class="button" [disabled]="hideNextButton" (click)="navigate.emit(1)">Next</button>
         </div>
         </div>
         </div>
@@ -31,44 +31,21 @@ export class ProgressbarComponent implements OnInit {
     @Input() answeredQuestions: AnswerValue[]; //ONLY REQUIRED QUESTIONS!!!
 
     @Output() navigate: EventEmitter<number> = new EventEmitter<number>();
-
-    questionNumber:number = 0;
-    amountOfQuestions:number[] = [];
-
-    allQuestions:number;
+    elems:any = []
     progBarLength:string = '0%';
-    answerProgress:any;
 
     ngOnInit(){
-        for(let i = 0; i < this.groups.length; i++) {
-            for(let x = 0; x < this.groups[i].elements.length; x++) {
-                this.amountOfQuestions.push(x);
-            }
-        }
-        this.allQuestions = this.amountOfQuestions.length;
-        this.answerProgress = (this.questionNumber / this.amountOfQuestions.length) * 100;
     }
+    ngOnChanges(changes: SimpleChanges) {
+        const group = this.groups.filter(g => g.uuid === this.currentGroup.uuid)[0];
+        const element = group.elements.filter(e => e.uuid === this.currentGroup.currentElement.uuid)[0];
+        /*const groupIdx = this.groups.indexOf(group);
+        const elemIdx = group.elements.indexOf(element);*/
 
-    questionNumberPlusOne(){
-        //console.log(this.answers);
-        let expanded = this.currentGroup.type.toString();
-        if(expanded != "expanded") {
-            this.questionNumber++;
-        } else {
-            this.questionNumber = this.questionNumber + 2;
-        }
-        this.answerProgress = (this.questionNumber / this.amountOfQuestions.length) * 100;
-        this.progBarLength = '' + this.answerProgress + '%';
+        this.elems = [];
+        for(let group of this.groups){ this.elems = this.elems.concat(group.elements); }
+        const currentPos = this.elems.indexOf(element);
+        console.log(this.elems.length);
+        this.progBarLength = (currentPos * (100/this.elems.length)) + '%';
     }
-    questionNumberMinusOne() {
-        let expanded = this.currentGroup.type.toString();
-        if(expanded != "expanded") {
-            this.questionNumber--;
-        } else {
-            this.questionNumber = this.questionNumber - 2;
-        }
-        this.answerProgress = (this.questionNumber / this.amountOfQuestions.length) * 100;
-        this.progBarLength = '' + this.answerProgress + '%';
-    }
-
 }
